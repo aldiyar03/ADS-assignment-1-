51e7f388e443007bdb1731a9d38368f2c224371a
aldiyar03 <240227@astanait.edu.kz>
2025-03-28 23:22:22 +0300

    Added function to calculate the average of elements in an array

---
+++ src/Task_2.java
@@ -0,0 +1,25 @@
+public class Task_2 {
+    /**
+     * This method calculates the average of the given array of numbers.
+     *
+     * Time complexity: O(n), where n is the number of elements in the array.
+     * The algorithm sums all elements in the array, then divides the sum by the array length.
+     *
+     * @param arr The array of numbers.
+     * @return The average of the elements in the array.
+     */
+    public static double findAverage(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        // Example usage of the findAverage method
+        int[] numbers = {4, 5, 6, 7};
+        double average = findAverage(numbers);
+        System.out.println("The average is: " + average);
+    }
+}
